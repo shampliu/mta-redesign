a0188492a2aac406db6f6e9e861136273df18ec1
Chang Liu <shampliu@gmail.com>
2017-02-09 02:25:20 -0800

    animejs animations

--- js/index.js
+++ js/index.js
@@ -6,4 +6,55 @@ document.addEventListener("DOMContentLoaded", function() {
 
     });
   });
+
+  setTimeout(init, 1000);
+
+
+
 });
+
+function init() {
+  anime({
+    targets: '.loader',
+    opacity: 0,
+    duration: 400,
+    easing: 'easeOutSine',
+    complete: function() {
+      anime({
+        targets: 'header.jumbotron',
+        opacity: 1,
+        duration: 500,
+        easing: 'easeOutSine',
+        complete: function() {
+          anime({
+            targets: 'nav.nav',
+            opacity: 1,
+            marginTop: ["-75px", "0px"],
+            easing: 'easeOutSine',
+            duration: 1200,
+            complete: function() {
+              anime({
+                targets: '.planner',
+                opacity: 1,
+                translateY: "-1rem",
+                easing: 'easeInOutQuad',
+                duration: 600,
+                complete: function() {
+                  anime({
+                    targets: '#getting-there > .theres > li',
+                    opacity: 1,
+                    delay: function(el, ind) {
+                      return ind * 150
+                    },
+                    easing: 'easeOutSine'
+                  })
+                }
+              })
+            }
+          })
+        }
+      })
+    }
+  });
+
+}
